cff53265a7f427b7e538c47221d942fc2d35dbd7
LuisalyCompres <luisalycompresrestituyo@gmail.com>
2024-03-27 11:48:39 -0400

    Nueva Actualizacion

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,40 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { ChangeEvent } from 'react';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+// Define the Search component with the placeholder property
+interface SearchProps {
+  placeholder: string;
+}
+
+export default function Search(props: SearchProps) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // Debounce the handleSearch function
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300); // Adjust debounce delay according to your preference
+
+  // Function to handle search
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const term = e.target.value;
+    handleSearch(term);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -10,9 +42,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
+        placeholder={props.placeholder}
+        onChange={handleChange}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
+
